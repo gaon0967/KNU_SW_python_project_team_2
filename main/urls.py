273e98c36f2d82a3ce0c217c0d59159f6fc4c309
오승윤 <timber44@naver.com>
2025-05-19 01:04:26 +0900

    traffic test

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('trafficAccident/', views.traffic_accident, name='trafficAccident'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'main/index.html')
+
+def traffic_accident(request):
+    return render(request, 'trafficAccident/mainPage.html')
