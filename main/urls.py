d59a413683a3d82c6d95b5c0629af68ff2ac6626
오승윤 <timber44@naver.com>
2025-05-19 21:17:21 +0900

    dashboard_2 완료했습니다. 실행하고 좌측 상단 버튼 누르시면 됩니다.

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('dashboard_2/', views.dashboard_2, name='dashboard_2'),
 ]
--- myproject/urls.py
+++ myproject/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from main import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('dashboard_2/', views.dashboard_2, name='dashboard_2'),
 ]
