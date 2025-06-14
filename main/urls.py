423df340d0f639fcde7a61d32638c439c885178e
gaoringring <gaon0967@naver.com>
2025-06-14 22:30:04 +0900

    Delete main directory

--- main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('dashboard_2/', views.dashboard_2, name='dashboard_2'),
-]
--- main/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-def home(request):
-    return render(request, 'main/dashboard_3.html')
-
-def dashboard_2(request):
-    return render(request, 'main/dashboard_2.html')
